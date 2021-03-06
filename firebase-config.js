4fc4760165467eb613740e628936b92ef7a51b0a
Michael Gagno <mgagno1@my.bcit.ca>
2021-03-06 00:35:04 -0800

    Add login support

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBKmJKOPPNTHfHBgYkZfv9AA-2pdbvsK3A",
+  authDomain: "bcit-calendar.firebaseapp.com",
+  projectId: "bcit-calendar",
+  storageBucket: "bcit-calendar.appspot.com",
+  messagingSenderId: "382761997867",
+  appId: "1:382761997867:web:43b0f743c0a700a8a93c6e"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
