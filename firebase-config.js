bc5cb294ec6d5efd3689be564659af9721718972
Michael Gagno <mgagno1@my.bcit.ca>
2021-03-23 14:12:18 -0700

    Put things in correct order

--- firebase-config.js
+++ firebase-config.js
@@ -7,6 +7,7 @@ var firebaseConfig = {
   messagingSenderId: "382761997867",
   appId: "1:382761997867:web:43b0f743c0a700a8a93c6e"
 };
-// Initialize Firebase
-const db = firebase.firestore();
+
 firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
