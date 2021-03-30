3fa55300007378a9ab63eba61a91a352dba1ffe7
Michael Gagno <mgagno1@my.bcit.ca>
2021-03-29 22:12:10 -0700

    Display full event details from database, based on id in url.

--- scripts/event.js
+++ scripts/event.js
@@ -2,10 +2,32 @@ let params = (new URL(document.location)).searchParams;
 let eventId = params.get("event");
 
 function showEventDetails(eventId) {
-    let event = db.collection("events").doc(eventId);
-    event.get().then((doc) => {
+    let eventInfo = db.collection("events").doc(eventId);
+    eventInfo.get().then((doc) => {
         if (doc.exists) {
             console.log("Document data:", doc.data());
+            let eventNameCourse = document.getElementById("event-name-course");
+            let eventName = document.getElementById("event-name");
+            let eventDescription = document.getElementById("event-description");
+            let eventDueDate = document.getElementById("event-due");
+            let eventLinks = document.getElementById("event-links");
+            let eventAnchor = document.getElementById("event-url");
+
+            let courseNameText = document.createTextNode(doc.data().course_name);
+            eventNameCourse.appendChild(courseNameText);
+
+            let eventNameText = document.createTextNode(doc.data().name);
+            eventName.appendChild(eventNameText);
+
+            let eventDescriptionText = document.createTextNode(doc.data().description);
+            eventDescription.appendChild(eventDescriptionText);
+
+            let eventDueDateText = document.createTextNode(doc.data().due_date);
+            eventDueDate.appendChild(eventDueDateText);
+
+            let eventLinksText = document.createTextNode(doc.data().link);
+            eventLinks.appendChild(eventLinksText);
+            eventAnchor.href = doc.data().link
         } else {
             console.log("No such document!");
         }
