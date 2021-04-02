42033be6af540f01b3e1b4d4282ed5e6a3cf3942
Jashanpreet Singh <jashan100802@gmail.com>
2021-04-03 02:39:47 +0530

    Assignment list fully working

--- scripts/admin_assignment_list.js
+++ scripts/admin_assignment_list.js
@@ -1,74 +1,64 @@
 const assign_list = document.querySelector('.assign_list');
 
 // create elements and rendef events list
+let inner_values = document.getElementsByClassName("inner_list");
 
-function renderEvent(doc){
-  let li = document.createElement('li');
-  let data_list = document.createElement('ul')
-  data_list.setAttribute('class', 'data_list')
 
-  let due_date = document.createElement('div');
-  let course_name = document.createElement('div');
-  let assignment_name = document.createElement('div');
-  let link = document.createElement('div');
-  let set = document.createElement('div');
-  
-  li.setAttribute('data-id', doc.id);
-  course_name.textContent = 'Name : '+ doc.data().course_name + '\n';
-  assignment_name.textContent ='Assignment name : ' + doc.data().name + '\n';
-  link.textContent = 'Link : '+ doc.data().link + '\n';
-  set.textContent = 'Set : '+ doc.data().set_number + '\n';
-  
-  li.textContent = 'Due-Date : '+ doc.data().due_date + '\n';
-  data_list.appendChild(due_date);
-  data_list.appendChild(course_name);
-  data_list.appendChild(assignment_name);
-  data_list.appendChild(link);
-  data_list.appendChild(set);
-  li.appendChild(data_list)
-  assign_list.appendChild(li);
-  
+function addlistListener(id) {
+
+  document.getElementById(id)
+      .addEventListener("click", function () {
+          console.log(id + "was clicked!")
+          //window.location.href="details.html";
+          window.location.href = "edit_event.html?id=" + id;
+
+      });
 }
 
 
-
-db.collection('events').orderBy('due_date').get().then((snapshot) => {
-  snapshot.docs.forEach(doc => {
-    //renderEvent(doc);
-
-  });
-
-})
-
-
-
-
-
-
 function displayList() {
-
     db.collection("events")
     .orderBy("due_date")
         .get() //READ asynch
         .then(function (snapcollection) {
             snapcollection.forEach(function (doc) {
+
                 console.log(doc.data())
                 console.log(doc.data().name);
                 var id = doc.id;
                 var name = doc.data().name;
-                $(".assign_list").append("<ol id=" + id + ">" + name + "</ol>")
+                var coursename = doc.data().course_name;
+                var duedate = doc.data().due_date;
+                
+                let inner_container = document.createElement("div")
+                let assign_list = document.querySelector(".assign_list")
+                let inner_list = document.createElement("li")
+                let value1 = document.createElement("ul")
+                let value2 = document.createElement("ul")
+
+                inner_list.setAttribute("id", id)
+                inner_container.setAttribute("class", "inner_container")
+
+                inner_list.textContent = duedate
+                value1.textContent = coursename
+                value2.textContent = name
+
+                inner_list.appendChild(value1)
+                inner_list.appendChild(value2)
+                inner_container.appendChild(inner_list)
+                assign_list.appendChild(inner_container)
+
                 addlistListener(id);
-            })
-        })
-}
+            })})}
+            
 displayList();
 
 
-function addlistListener(id) {
-    document.getElementById(id)
-        .addEventListener("click", function () {
-            console.log(id + "was clicked!")
-            //window.location.href="details.html";
-            window.location.href = "edit_event.html?id=" + id;
-        });
-}
+
+
+
+
+
+
+
+
