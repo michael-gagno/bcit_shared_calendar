e41dbd524f62d751051e84c585bb5ec1f12bca28
Jashanpreet Singh <jashan100802@gmail.com>
2021-04-07 07:13:32 +0530

    removed redundant functions

--- scripts/assignment_list.js
+++ scripts/assignment_list.js
@@ -13,36 +13,6 @@ function addlistListener(id) {
       });
 }
 
-
-function displayList() {
-    db.collection("events")
-    .orderBy("due_date")
-        .get() //READ asynch
-        .then(function (snapcollection) {
-            snapcollection.forEach(function (doc) {
-                console.log(doc.data())
-                console.log(doc.data().name);
-                var id = doc.id;
-                var name = doc.data().name;
-                var coursename = doc.data().course_name;
-                var duedate = doc.data().due_date;
-
-                let assign_list = document.querySelector(".assign_list")
-                let inner_list = document.createElement("li")
-                inner_list.setAttribute("id", id)
-                let value1 = document.createElement("ul")
-                let value2 = document.createElement("ul")
-                inner_list.textContent = duedate
-                value1.textContent = coursename
-                value2.textContent = name
-                inner_list.appendChild(value1)
-                inner_list.appendChild(value2)
-                assign_list.appendChild(inner_list)
-                addlistListener(id);
-            })})}
-            
-//displayList();
-
 function displayCard() {
     db.collection("events")
     .orderBy("due_date")
@@ -105,31 +75,13 @@ function displayCard() {
                 }
                 else if (status !== "complete"){
                     completion_image.style.display = 'none'
-                    mark_as_complete_btn.style.display = 'block'
-                   // if(mark_as_complete_btn.click){
-                       // console.log("button clicked")
-                   // }
                 }
+
                 mark_as_complete_btn.addEventListener("click", function(event){
                     console.log("clicked")
                     localStorage.setItem(id, "complete");
                     location.reload();
                 })
-                
-                //storage.setItem(id, 'complete');
-                
-
-                
-                //let inner_list = document.createElement("li")
-                //inner_list.setAttribute("id", id)
-                //let value1 = document.createElement("ul")
-                //let value2 = document.createElement("ul")
-                //inner_list.textContent = duedate
-                //value1.textContent = coursename
-                //value2.textContent = name
-                //inner_list.appendChild(value1)
-                //inner_list.appendChild(value2)
-                //assign_list.appendChild(inner_list)
                 
             })})}
 
