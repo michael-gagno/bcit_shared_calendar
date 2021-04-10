9a757436e99c62fde71d2677f52369b9fd7913f5
Jashanpreet Singh <jashan100802@gmail.com>
2021-04-10 09:08:04 +0530

    added default set as A

--- scripts/change_set.js
+++ scripts/change_set.js
@@ -1,9 +1,14 @@
 let save_set_btn = document.getElementById("save-set")
 save_set_btn.addEventListener('click', function(event){
     set_value = get_value_from_radio_btn()
-    localStorage.setItem("set", set_value)
-    alert("Your set is now changed to "+ localStorage.getItem("set"))
-    window.location.href = "assignment_list.html"
+    if (set_value == null){
+        localStorage.setItem("set", "A")
+    }
+    else{
+        localStorage.setItem("set", set_value)
+        alert("Your set is now changed to "+ localStorage.getItem("set"))
+        window.location.href = "assignment_list.html"
+    }
 });
 
 
