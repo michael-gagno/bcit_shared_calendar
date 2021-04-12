84d2a5515407ea1e570636e8a6986c0bf01c8aaa
Jason Banh <jbanh2@gmail.com>
2021-04-12 03:11:15 -0700

    changed admin page to display username

--- scripts/admin_auth.js
+++ scripts/admin_auth.js
@@ -7,3 +7,21 @@ firebase.auth().onAuthStateChanged(function(user) {
       window.location.replace("./account.html");
     }
   });
+
+  function AdminWelcome() {
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            console.log(user.uid);
+            db.collection("users").doc(user.uid)
+                .get()
+                .then(function (doc) {
+                    var username = doc.data().name;
+                    console.log(username);
+                    document.getElementById("username").innerText = "Welcome, " + username;
+                })
+        } else {
+            // Nothing
+        }
+    });
+}
+AdminWelcome();
